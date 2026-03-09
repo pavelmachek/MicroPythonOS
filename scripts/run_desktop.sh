c194e9a8c2fd16354e0217123a2a46a543b8182e
Pavel Machek <pavel@ucw.cz>
2026-03-09 12:05:08 +0100

    fix: directory does not exist on freshly checked-out repository

--- scripts/run_desktop.sh
+++ scripts/run_desktop.sh
@@ -37,7 +37,8 @@ if [ -f "$script" ]; then
 	echo "Running script $script"
 	"$binary" -v -i "$script"
 else
-	CONFIG_FILE="data/com.micropythonos.settings/config.json"
+	CONFIG_DIR="data/com.micropythonos.settings/
+	CONFIG_FILE="$CONFIG_DIR/config.json"
 	if [ -n "$script" ]; then
 		echo "run_desktop.sh: running app $script"
 		if [ -f "$CONFIG_FILE" ]; then
